5d84f74907a0352d0e53d2fc49584c6007d92cb7
Christoph <christoph.gnip@gmail.com>
2018-09-04 14:44:47 -0700

    :sparkles: Craete Home Channel; Use Channel

--- phx_kiosk/lib/phx_kiosk_web/channels/user_socket.ex
+++ phx_kiosk/lib/phx_kiosk_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PhxKioskWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PhxKioskWeb.RoomChannel
+  channel "home:lobby", PhxKioskWeb.HomeChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
